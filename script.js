3c877b3b3dec741be8cc019ccca35c77c6493064
npansuriya17 <73963997+npansuriya17@users.noreply.github.com>
2021-01-17 14:02:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelector("#compute").addEventListener("click", () => {
+    var btn = document.querySelector("#compute").addEventListener("click", () => {
         
         var principal = document.querySelector("#principal").value;
-        if (int(principal) <= 0) {
+        if (principal <= 0) {
             alert("Enter a positive number");
             document.querySelector("#principal").focus();
         }
 
-        var rate = float(document.querySelector("#rate").value);
-        var years = int(document.querySelector("#years").value);
-        
+        var rate = document.querySelector("#rate").value;
+        var years = document.querySelector("#years").value;
 
         var interest = principal * rate * years / 100;
-        var year = 2020 + years;
-        var result = 'If you deposit' + principal + ', at an interest rate of ' + rate + '%. You will receive an amount of ' + interest + ', in the year ' + year;
-        document.querySelector('#result').innerhtml = result;
+        amount = Number(principal) + Number(interest);
+        var year = Number(2020) + Number(years);
+        document.querySelector('#result').textContent = 'If you deposit ' + principal + ', at an interest rate of ' + rate + '%. You will receive an amount of ' + amount + ', in the year ' + year;
         
     });
 
-    document.querySelector("#rate").addEventListener("input", () => {
+    var rate_val = document.querySelector("#rate").addEventListener("input", () => {
         var val = document.querySelector('#rate').value;
-        document.querySelector('#int_val').innerhtml = val + '%';
+        document.querySelector("#int_val").textContent = val + '%';
     });
 });
